c12725e1c2d6b8b9d0261dc274b905cc4a0ba32c
CristRod <cristianrodriguesouza@gmail.com>
2023-10-17 21:57:08 -0300

    fix: updating BinarySearch

--- data_structure/BinarySearch/Main.java
+++ data_structure/BinarySearch/Main.java
@@ -1,5 +1,44 @@
 package data_structure.BinarySearch;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Integer[] vetor = new Integer[1000];
+
+            for(int i = 0; vetor.length > i; i++) {
+                vetor[i] = i * 2;
+            }
+
+            System.out.println("Choose a number");
+            Scanner scanner = new Scanner(System.in);
+
+            int count = 0;
+            int findit = scanner.nextInt();
+            Boolean find = false;
+            int start = 0;
+            int end = vetor.length - 1;
+            int mid;
+
+            while(start <= end) {
+                mid = (int) ((start + end) / 2);
+                count++;    
+                if(vetor[mid] == findit) {
+                        find = true;
+                        break;
+                    } else if(vetor[mid] < findit){
+                        start = mid + 1;
+                    } else {
+                        end = mid - 1;
+                    }
+            }
+
+            if(find == true) {
+                System.out.println("u find it" + ", " + count );
+            } else {
+                System.out.println("u didnt find it");
+            }
+
+    }
     
 }
